0dc276ca3271cecc38874e16b5e4bb5120ea8b3d
vagrant <vagrant@precise32.(none)>
2014-02-05 23:09:23 +0000

    Putting it together lab, v1. possible stretch goal next

--- public/js/hci-friends.js
+++ public/js/hci-friends.js
@@ -10,6 +10,11 @@ $(document).ready(function() {
  */
 function initializePage() {
 	console.log("Javascript connected!");
+	$('.friend-name').click( function(e) {
+		e.preventDefault();
+		var newName = anagrammedName($(this).text());
+		$(this).text(newName);
+	});
 }
 
 function anagrammedName(name) {
--- routes/add.js
+++ routes/add.js
@@ -1,5 +1,12 @@
 var data = require("../data.json");
 
-exports.addFriend = function(req, res) {    
-	// Your code goes here
+exports.addFriend = function(req, res) { 
+	var newFriend = {
+		'name': req.query.name,
+		'description': req.query.description,
+		'imageURL': 'http://lorempixel.com/400/400/people'
+	};
+	data['friends'].push(newFriend);
+	console.log(data);
+	res.render('add');
  }
